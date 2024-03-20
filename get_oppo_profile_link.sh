85c856e94aa37ac8ca43010c9edef9badfc3a8dc
Funecio-Agrante <164108129+Funecio-Agrante@users.noreply.github.com>
2024-03-20 00:47:42 +0000

    Create get_oppo_profile_link.sh
    This script scans your local Gods Unchained debug.log file and creates a link for your opponent's https://gudecks.com/ profile page, where you can find the most probable deck list they are queuing up against you.

---
+++ get_oppo_profile_link.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+debug_file="/path/to/debug.log"
+
+tail -F $debug_file 2>&1 | while read -r line; do
+    
+    if [ "$start" = false ]; then
+        grep -q "has appeared;  following new file"
+        start=true
+    elif [ -z $mode ]; then
+        mode=$(echo "$line" | sed -n "s/.*Game mode is '\([0-9]\+\)'.*/\1/p")
+    else
+        oppo_id=$(echo "$line" | sed -n 's/.*, o:PlayerInfo(apolloId: \([0-9]\+\),.*/\1/p')
+        if [ -n "$oppo_id" ]; then
+            clear
+            echo "https://gudecks.com/meta/player-stats?gameMode=$mode&userId=$oppo_id"
+            start=false
+        fi
+    fi
+done
